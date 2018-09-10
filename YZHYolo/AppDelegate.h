40fb766981f0deb3570fd4a1611c776bd4188a50
Jersey <shi.li@yzhchain.com>
2018-09-10 11:43:59 +0800

    Project init.

---
+++ YZHYolo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZHYolo
+//
+//  Created by 😘王艳 on 2018/9/10.
+//  Copyright © 2018年 YZHChain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZHYolo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YZHYolo
+//
+//  Created by 😘王艳 on 2018/9/10.
+//  Copyright © 2018年 YZHChain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
